8981d0db09e5e31a37eb7861e51bc6356b34e474
gloriaJun <pureainu@gmail.com>
2019-10-10 23:31:35 +0900

    chore: Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const glob = require('glob');
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const generateHTMLPlugins = () => glob.sync('./src/**/*.html').map(
+    dir => new HtmlWebpackPlugin({
+        filename: path.basename(dir), // Output
+        template: dir, // Input
+    }),
+);
+
+module.exports = {
+    mode: 'development',
+    stats: {
+        colors: true,
+    },
+    devtool: 'source-map',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+        contentBase: 'src',
+        watchContentBase: true,
+        hot: true,
+        open: true,
+        port: process.env.PORT || 9000,
+        host: process.env.HOST || 'localhost',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: './src/static/',
+                to: './static/',
+            },
+        ]),
+        ...generateHTMLPlugins(),
+    ]
+};
